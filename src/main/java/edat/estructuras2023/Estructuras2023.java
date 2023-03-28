390069005974b4b395eed0e7acdc92e4f654f911
Kolia561 <nicolas.blanco@est.fi.uncoma.edu.ar>
2023-03-28 15:42:25 -0300

    first commit

---
+++ src/main/java/edat/estructuras2023/Estructuras2023.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edat.estructuras2023;
+
+/**
+ *
+ * @author GilgameshRyzen
+ */
+public class Estructuras2023 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
