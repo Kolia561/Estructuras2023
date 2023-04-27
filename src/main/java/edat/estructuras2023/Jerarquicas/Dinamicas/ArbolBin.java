ffa277b1c80912e92929f56b660cf4535bf2a718
Nicolas Blanco <nicolas.blanco@est.fi.uncoma.edu.ar>
2023-04-27 20:00:02 +0000

    ArbolBin: ListarInOrden, ListarPosOrden, ListarPreOrden. Altura, Nivel, Padre

--- src/main/java/edat/estructuras2023/Jerarquicas/Dinamicas/ArbolBin.java
+++ src/main/java/edat/estructuras2023/Jerarquicas/Dinamicas/ArbolBin.java
@@ -1,5 +1,7 @@
 package edat.estructuras2023.Jerarquicas.Dinamicas;
 
+import edat.estructuras2023.Lineales.Dinamicas.Lista;
+
 public class ArbolBin {
 
     private NodoArbol raiz;
@@ -26,7 +28,7 @@ public class ArbolBin {
                 if (Character.toLowerCase(lugar) == 'i'  && nodoPadre.getIzquierdo() == null) {
                     nodoPadre.setIzquierdo(new NodoArbol(nuevoElem, null, null));
                 rta = true;
-                } else if (lugar == 'D' && nodoPadre.getDerecho() == null) {
+                } else if (Character.toLowerCase(lugar) == 'd' && nodoPadre.getDerecho() == null) {
                     nodoPadre.setDerecho(new NodoArbol(nuevoElem, null, null));
                 rta = true;
                 }
@@ -75,35 +77,151 @@ public class ArbolBin {
     }
 
     public int altura(){
-        int rta=-1;
+        int rta;
 
-        if (this.raiz !=null){
+       
             rta=alturaAux(this.raiz);
-        }
+        
 
         return rta;
     }
 
     private int alturaAux(NodoArbol nodo){
-        int rta=0;
+        int rta;
+        int altIzq, altDer;
 
-        if (nodo !=null){
-            rta=1+Math.max(alturaAux(nodo.getIzquierdo()), alturaAux(nodo.getDerecho()));
+        if (nodo ==null){
+            rta=-1;
+        }else{
+            // rta = 1 + Math.max(alturaAux(nodo.getIzquierdo()), alturaAux(nodo.getDerecho())
+
+            altDer=alturaAux(nodo.getDerecho());
+
+            altIzq=alturaAux(nodo.getIzquierdo());
+            
+            if(altDer>altIzq){
+                rta=altDer+1;
+            }else{
+                rta=altIzq+1;
+            }
+           
         }
 
         return rta;
     }
 
-    public NodoArbol padre(){
+    public NodoArbol padre(NodoArbol elem){
 
         NodoArbol rta=null;
 
+        rta=padreAux(this.raiz, elem);
+        
+        return rta;
+    }
 
+    public NodoArbol padreAux(NodoArbol nodo, NodoArbol elem){
+        
+        NodoArbol rta=null;
 
-
+        if(nodo!=null){
+            //si el nodo no es nulo, comprueba si el hijo izquierdo o derecho contiene elemento buscado
+            if(nodo.getIzquierdo().getElem().equals(elem) || nodo.getDerecho().getElem().equals(elem)){
+                //si lo contiene, devuelve el nodo
+                rta=nodo;
+            }else{ //si no lo contiene, busca en el hijo izquierdo y derecho
+                rta=padreAux(nodo.getIzquierdo(), elem);
+                //si no lo encuentra en el hijo izquierdo, busca en el derecho
+                if(rta==null){
+                    rta=padreAux(nodo.getDerecho(), elem);
+                }
+            }
+        }
 
         return rta;
     }
 
+    public int nivel(NodoArbol elem){
+        int rta;
+
+       rta=alturaAux(obtenerNodo(this.raiz, elem));
+
+        return rta;
+    }
+
+    public Lista listarPreorden(){
+        Lista rta=new Lista();
+        return listarPreordenAux(this.raiz,rta);
+    }
+
+    private Lista listarPreordenAux(NodoArbol nodo, Lista lista){
+        
+        if(nodo!=null){
+
+        lista.insertar(nodo.getElem(), lista.longitud()+1);
+        
+        lista = listarPreordenAux(nodo.getDerecho(), lista);
+        lista = listarPreordenAux(nodo.getIzquierdo(), lista);
+
+        }
+
+        return lista;
+    }
+
+    public Lista listarInorden(){
+        Lista rta=new Lista();
+        return listarInordenAux(this.raiz,rta);
+    }
+
+    private Lista listarInordenAux(NodoArbol nodo, Lista lista){
+        
+        if(nodo!=null){
+
+        lista = listarInordenAux(nodo.getDerecho(), lista);
+
+        lista.insertar(nodo.getElem(), lista.longitud()+1);
+
+        lista = listarInordenAux(nodo.getIzquierdo(), lista);
+
+        }
+
+        return lista;
+    }
+
+    public Lista listarPosorden(){
+        Lista rta=new Lista();
+        return listarPosordenAux(this.raiz,rta);
+    }
+
+    private Lista listarPosordenAux(NodoArbol nodo, Lista lista){
+        
+        if(nodo!=null){
+
+        lista = listarPosordenAux(nodo.getDerecho(), lista);
+
+        lista = listarPosordenAux(nodo.getIzquierdo(), lista);
+
+        lista.insertar(nodo.getElem(), lista.longitud()+1);
+
+        }
+
+        return lista;
+    }
+
+    public Lista listarPorNiveles(){
+        Lista rta=new Lista();
+        return listarPorNivelesAux(this.raiz,rta);
+    }
+
+    private Lista listarPorNivelesAux(NodoArbol nodo, Lista lista){
+        
+        if(nodo!=null){
+        
+        }
+
+        return lista;
+    }
+
+
 
 }
+
